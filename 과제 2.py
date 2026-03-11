0f93b6b4985441743cf1f0a4d1cc1718ccac2229
sys005811-prog <sys005811@gmail.com>
2026-03-11 22:39:06 +0900

    Add files via upload

---
+++ 과제 2.py
@@ -0,0 +1,76 @@
+#2-1
+print('나의 이름은:','성영수')
+print('나의 나이는:', 24)
+print('나의 키는', 168, 'cm 입니다.')
+print('10 + 20=', 10+20)
+print('10*20=',10*20)
+
+#2-4
+name='성영수'
+print('나의 이름은:', name)
+
+age=24
+print('나의 나이는:', age)
+
+height=168
+print('나의 키는', height,'cm 입니다')
+
+sum=10+20
+print('10+20=',sum)
+
+mult=10*20
+print('10*20=', mult)
+
+#2-6
+width=20
+heigth=40
+width=30
+area=width*heigth
+print('사각형의 면적',area)
+
+#2-7
+print('1)',123*456,
+      '2)',1357+2468,
+      '3)',5**4,
+      '4)',10/4,
+      '5)',10//5,
+      '6)',10%5)
+print(5%2)
+print(2**0.5, 3**0.5)
+
+#2-8
+a=8+2j
+b=4+3j
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#2-9
+a=1024
+print('(1)', a>>1)
+print('(2)', a>>2)
+a=a>>1
+print('(3)', a)
+a=a>>1
+print('(4)', a)
+a=1
+print('(5)', a<<1)
+a=a<<1
+print('(6)', a)
+a=a<<1
+print('(7)', a)
+a=a<<1
+print('(8)', a)
+a=a<<1
+print('(9)', a)
+
+#2-10
+name=input('이름을 입력하세요:')
+print(name, '님이 입장하셨습니다.')
+
+m=int(input('숫자 m을 입력하세요:'))
+n=int(input('숫자 n을 입력하세요:'))
+print('m+n=', m+n)
+print('m-n=', m-n)
+
